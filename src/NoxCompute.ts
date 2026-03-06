c1e720e359949e77741390a3677077c60b56eeaa
Le-Caignec <robinlecaignec@yahoo.fr>
2026-03-06 14:54:51 +0100

    refactor: move handle and role functions to utils for better organization

--- src/NoxCompute.ts
+++ src/NoxCompute.ts
@@ -1,42 +1,9 @@
-import { BigInt, Bytes } from '@graphprotocol/graph-ts';
 import {
     Allowed as AllowedEvent,
     MarkedAsPubliclyDecryptable as MarkedAsPubliclyDecryptableEvent,
     ViewerAdded as ViewerAddedEvent,
 } from '../generated/NoxCompute/NoxCompute';
-import { Handle, HandleRole } from '../generated/schema';
-
-function getOrCreateHandle(handleId: Bytes): Handle {
-    let handle = Handle.load(handleId);
-    if (handle == null) {
-        handle = new Handle(handleId);
-        handle.isPubliclyDecryptable = false;
-        handle.save();
-    }
-    return handle;
-}
-
-function createRole(
-    handle: Handle,
-    account: Bytes,
-    role: string,
-    grantedBy: Bytes,
-    txHash: Bytes,
-    logIndex: i32,
-    blockNumber: BigInt,
-    blockTimestamp: BigInt,
-): void {
-    const roleId = txHash.concatI32(logIndex);
-    const handleRole = new HandleRole(roleId);
-    handleRole.handle = handle.id;
-    handleRole.account = account;
-    handleRole.role = role;
-    handleRole.grantedBy = grantedBy;
-    handleRole.blockNumber = blockNumber;
-    handleRole.blockTimestamp = blockTimestamp;
-    handleRole.transactionHash = txHash;
-    handleRole.save();
-}
+import { createRole, getOrCreateHandle } from './utils/utils';
 
 export function handleAllowed(event: AllowedEvent): void {
     const handle = getOrCreateHandle(event.params.handle);
